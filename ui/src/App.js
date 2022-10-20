de9b04976266d05ad7e7d867cc4fe4365455fd79
Matthew Greenup <matthewgreenup@Green-Machine.home>
2022-10-20 15:39:09 -0700

    navbar doesn't show up not logged in

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,65 +1,65 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import Home from './Components/Home.js';
-import { MemberDetails } from './Components/MembersDetail.js';
-import { Settings } from './Components/Settings.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { MemberContext } from './Components/MemberContext.js';
-import PersistentDrawerLeft from './Components/Navbar.jsx';
-import { DataSources } from './Components/DataSources.js';
-import IndividualMember from './Components/InvidualMember.js';
-import SignIn from './Components/SignIn.jsx';
-import SignUp from './Components/SignUp';
-import { useCookies } from 'react-cookie';
+import React, { useState, useEffect, useMemo } from 'react'
+import Home from './Components/Home.js'
+import { MemberDetails } from './Components/MembersDetail.js'
+import { Settings } from './Components/Settings.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { MemberContext } from './Components/MemberContext.js'
+import PersistentDrawerLeft from './Components/Navbar.jsx'
+import { DataSources } from './Components/DataSources.js'
+import IndividualMember from './Components/InvidualMember.js'
+import SignIn from './Components/SignIn.jsx'
+import SignUp from './Components/SignUp'
+import { useCookies } from 'react-cookie'
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [member, setMember] = useState([]);
-  const [usersArray, setUsersArray] = useState([]);
-  const [triggerFetch, setTriggerFetch] = useState(false);
-  const [toggle, setToggle] = useState(false); //for alerts
-  const [toggler, setToggler] = useState(false); //for alerts
-  const [postAlert, setPostAlert] = useState(false);
-  const [allWeapons, setAllWeapons] = useState([]);
-  const [allFlights, setAllFlights] = useState([]);
-  const [cookies, setCookie, removeCookie] = useCookies(['auth', 'user']);
-  const [userAccount, setUserAccount] = useState(null);
+  const [data, setData] = useState([])
+  const [member, setMember] = useState([])
+  const [usersArray, setUsersArray] = useState([])
+  const [triggerFetch, setTriggerFetch] = useState(false)
+  const [toggle, setToggle] = useState(false) //for alerts
+  const [toggler, setToggler] = useState(false) //for alerts
+  const [postAlert, setPostAlert] = useState(false)
+  const [allWeapons, setAllWeapons] = useState([])
+  const [allFlights, setAllFlights] = useState([])
+  const [cookies, setCookie, removeCookie] = useCookies(['auth', 'user'])
+  const [userAccount, setUserAccount] = useState(null)
 
-  const API = 'http://localhost:8080';
+  const API = 'http://localhost:8080'
   // const API = 'https://api.cyberhelm.com';
 
   useMemo(() => {
     if (cookies.user) {
-      setUserAccount(cookies.user);
+      setUserAccount(cookies.user)
       // console.log('cookies ', cookies);
     }
-  }, [cookies]);
+  }, [cookies])
 
   useEffect(() => {
     fetch(`${API}/users`, {
-      method: 'GET',
+      method: 'GET'
     })
       .then(res => res.json())
       .then(data => setData(data))
-      .catch(err => console.log(err));
-  }, [API, triggerFetch]);
+      .catch(err => console.log(err))
+  }, [API, triggerFetch])
 
   useEffect(() => {
     fetch(`${API}/allweapons`, {
-      method: 'GET',
+      method: 'GET'
     })
       .then(res => res.json())
       .then(data => setAllWeapons(data))
-      .catch(err => console.log(err));
-  }, [API]);
+      .catch(err => console.log(err))
+  }, [API])
 
   useEffect(() => {
     fetch(`${API}/flight`, {
-      method: 'GET',
+      method: 'GET'
     })
       .then(res => res.json())
       .then(data => setAllFlights(data))
-      .catch(err => console.log(err));
-  }, [API]);
+      .catch(err => console.log(err))
+  }, [API])
 
   // useEffect(() => {
   //   console.log('user account ', userAccount);
@@ -87,23 +87,28 @@ const App = () => {
     removeCookie,
     userAccount,
     setUserAccount,
-    allFlights,
-  };
+    allFlights
+  }
 
   return (
     <MemberContext.Provider value={obj}>
       <Router>
-        <PersistentDrawerLeft />
         {userAccount !== null ? (
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<SignIn />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/data' element={<DataSources />} />
-            <Route path='/sfmembers' element={<MemberDetails />} />
-            <Route path='/sfmembers/:memberId' element={<IndividualMember />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
+          <>
+            <PersistentDrawerLeft />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<SignIn />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/data' element={<DataSources />} />
+              <Route path='/sfmembers' element={<MemberDetails />} />
+              <Route
+                path='/sfmembers/:memberId'
+                element={<IndividualMember />}
+              />
+              <Route path='/settings' element={<Settings />} />
+            </Routes>
+          </>
         ) : (
           <Routes>
             <Route path='/' element={<SignIn />} />
@@ -117,7 +122,7 @@ const App = () => {
         )}
       </Router>
     </MemberContext.Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
