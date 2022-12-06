37a655a44262f0bc253bccf84132ad7679997781
matthewgreenup <108901188+matthewgreenup@users.noreply.github.com>
2022-12-06 11:51:11 -0800

    added status to profiles

--- api/db/migrations/03_user_table.js
+++ api/db/migrations/03_user_table.js
@@ -4,22 +4,23 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('user_table', table => {
-    table.increments('id');
-    table.string('first_name', 250);
-    table.string('last_name', 250);
-    table.string('rank', 250);
-    table.string('flight');
-    table.foreign('flight').references('flight').inTable('flight');
-    table.integer('cert_id');
-    table.foreign('cert_id').references('id').inTable('certification');
-    table.boolean('weapon_arming');
-    table.boolean('admin').defaultTo(false);
-    table.string('notes', 1024);
-    table.string('email');
-    table.string('password');
-    table.string('avatar');
-  });
-};
+    table.increments('id')
+    table.string('first_name', 250)
+    table.string('last_name', 250)
+    table.string('rank', 250)
+    table.string('flight')
+    table.foreign('flight').references('flight').inTable('flight')
+    table.integer('cert_id')
+    table.foreign('cert_id').references('id').inTable('certification')
+    table.boolean('weapon_arming')
+    table.boolean('admin').defaultTo(false)
+    table.string('notes', 1024)
+    table.string('email')
+    table.string('password')
+    table.string('avatar')
+    table.string('status')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
@@ -28,10 +29,10 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema
     .alterTable('user_table', table => {
-      table.dropForeign('flight');
-      table.dropForeign('cert_id');
+      table.dropForeign('flight')
+      table.dropForeign('cert_id')
     })
     .then(() => {
-      return knex.schema.dropTableIfExists('user_table');
-    });
-};
+      return knex.schema.dropTableIfExists('user_table')
+    })
+}
