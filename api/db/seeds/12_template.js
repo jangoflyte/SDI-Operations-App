f0268193b4797ad2691f9424827a00b9643a9b98
matthewgreenup <108901188+matthewgreenup@users.noreply.github.com>
2023-03-07 08:35:15 -0800

    seeds

--- api/db/seeds/12_template.js
+++ api/db/seeds/12_template.js
@@ -209,11 +209,23 @@ exports.seed = async function (knex) {
         }
       ])
     },
+    ////////////////////////////////////////////////////////////////////
     {
       name: '8 Hours',
       post_array: JSON.stringify([
+        // {
+        //   post_id: 2,
+        //   man_req: 1,
+        //   cert_id: 3,
+        //   shift: 'days',
+        //   weapon_req: [1, 2],
+        //   flight_assigned: 1,
+        //   start_datetime: '2023-06-20T06:00',
+        //   shift_duration: 8
+        // },
+
         {
-          post_id: 2,
+          post_id: 1,
           man_req: 1,
           cert_id: 3,
           shift: 'days',
@@ -221,11 +233,111 @@ exports.seed = async function (knex) {
           flight_assigned: 1,
           start_datetime: '2023-06-20T06:00',
           shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 2,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 3,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 4,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 5,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 6,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 7,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 8,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 9,
+          man_req: 2,
+          cert_id: 2,
+          shift: 'days',
+          weapon_req: [1, 2],
+          flight_assigned: 1,
+          start_datetime: '2023-06-20T06:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
         },
 
         // Swings ---------------------------------
+        // {
+        //   post_id: 2,
+        //   man_req: 1,
+        //   cert_id: 3,
+        //   shift: 'swings',
+        //   weapon_req: [1, 2],
+        //   flight_assigned: 3,
+        //   start_datetime: '2023-06-20T14:00',
+        //   shift_duration: 8
+        // },
+
         {
-          post_id: 2,
+          post_id: 1,
           man_req: 1,
           cert_id: 3,
           shift: 'swings',
@@ -233,10 +345,110 @@ exports.seed = async function (knex) {
           flight_assigned: 3,
           start_datetime: '2023-06-20T14:00',
           shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
         },
-        // Mids ---------------------------------
         {
           post_id: 2,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 3,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 4,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 5,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 6,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 7,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 8,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 9,
+          man_req: 2,
+          cert_id: 2,
+          shift: 'swings',
+          weapon_req: [1, 2],
+          flight_assigned: 3,
+          start_datetime: '2023-06-20T14:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+
+        // Mids ---------------------------------
+        // {
+        //   post_id: 2,
+        //   man_req: 1,
+        //   cert_id: 3,
+        //   shift: 'mids',
+        //   weapon_req: [1, 2],
+        //   flight_assigned: 5,
+        //   start_datetime: '2023-06-20T22:00',
+        //   shift_duration: 8
+        // },
+        {
+          post_id: 1,
           man_req: 1,
           cert_id: 3,
           shift: 'mids',
@@ -244,6 +456,95 @@ exports.seed = async function (knex) {
           flight_assigned: 5,
           start_datetime: '2023-06-20T22:00',
           shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 2,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 3,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 4,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 5,
+          man_req: 2,
+          cert_id: 1,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 6,
+          man_req: 1,
+          cert_id: 4,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 7,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 8,
+          man_req: 1,
+          cert_id: 2,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
+        },
+        {
+          post_id: 9,
+          man_req: 2,
+          cert_id: 2,
+          shift: 'mids',
+          weapon_req: [1, 2],
+          flight_assigned: 5,
+          start_datetime: '2023-06-20T22:00',
+          shift_duration: 8
+          // end_datetime: '2023-06-20T18:00'
         }
       ])
     }
